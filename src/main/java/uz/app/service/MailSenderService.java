9f42bbee8b9c4467d3b2a09e72cbd1a0c6b39c82
Abror Abduganiev <developabror@gmail.com>
2024-06-20 12:02:18 +0500

    mailtrap setups

--- src/main/java/uz/app/service/MailSenderService.java
+++ src/main/java/uz/app/service/MailSenderService.java
@@ -4,25 +4,32 @@ import javax.mail.*;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
 import java.util.Properties;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class MailSenderService {
     private Properties properties = new Properties();
+
     {
-        properties.put("mail.smtp.host", "smtp.gmail.com");
-        properties.put("mail.smtp.port", "465");
-        properties.put("mail.smtp.ssl.enable", "true");
+        properties.put("mail.smtp.host", "sandbox.smtp.mailtrap.io");
+        properties.put("mail.smtp.port", "587");
+        properties.put("mail.smtp.starttls.enable", "true");
         properties.put("mail.smtp.auth", "true");
+        properties.put("mail.debug", "false");
     }
 
-    String senderMail = "email";
+    String senderMail = "mailtrapuser";
 
     private Session getSession() {
-        return Session.getInstance(properties, new Authenticator() {
+
+        Session session = Session.getInstance(properties, new Authenticator() {
             @Override
             protected PasswordAuthentication getPasswordAuthentication() {
-                return new PasswordAuthentication(senderMail, "app password");
+                return new PasswordAuthentication(senderMail, "mailtrap-password");
             }
         });
+        session.setDebug(false);
+        return session;
     }
 
     public void sendMessage(String to, String code) {
@@ -34,15 +41,16 @@ public class MailSenderService {
             message.setFrom(new InternetAddress(senderMail));
             message.setRecipient(Message.RecipientType.TO, new InternetAddress(to));
             Transport.send(message);
-            System.out.println("sent to "+to);
+            System.out.println("sent to " + to);
         } catch (Exception e) {
 //            e.printStackTrace();
         }
     }
 
     private static MailSenderService mailSenderService;
-    public static MailSenderService getInstance(){
-        if (mailSenderService == null){
+
+    public static MailSenderService getInstance() {
+        if (mailSenderService == null) {
             mailSenderService = new MailSenderService();
         }
         return mailSenderService;
