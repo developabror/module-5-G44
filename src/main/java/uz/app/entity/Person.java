21c887707fcba21c1101675538ccbf9ed37fbeb8
Abror Abduganiev <developabror@gmail.com>
2024-06-22 12:05:09 +0500

    changes

---
+++ src/main/java/uz/app/entity/Person.java
@@ -0,0 +1,11 @@
+package uz.app.entity;
+
+import lombok.Value;
+
+@Value
+public class Person {
+    String name;
+    String surname;
+    String gender;
+    String nationality;
+}
--- src/main/java/uz/app/entity/User.java
+++ src/main/java/uz/app/entity/User.java
@@ -1,68 +1,23 @@
 package uz.app.entity;
 
+
+import lombok.*;
+
+//@AllArgsConstructor
+//@NoArgsConstructor
+//@Builder
+//@Data
+//@Setter
+//@Getter
+//@ToString
+//@EqualsAndHashCode(of = {"username", "age"})
+
 public class User {
-    private String name;
-    private String email;
+    @NonNull
+    private String username;
+    @NonNull
     private String password;
-    private String code;
-    private boolean confirmed;
+    private String age;
+    private String birthDate;
 
-    public User(){
-
-    }
-
-    public User(String name, String email, String password) {
-        this.name = name;
-        this.email = email;
-        this.password = password;
-    }
-
-    public String getCode() {
-        return code;
-    }
-
-    public void setCode(String code) {
-        this.code = code;
-    }
-
-    public boolean isConfirmed() {
-        return confirmed;
-    }
-
-    public void setConfirmed(boolean confirmed) {
-        this.confirmed = confirmed;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    @Override
-    public String toString() {
-        return "User{" +
-                "name='" + name + '\'' +
-                ", email='" + email + '\'' +
-                ", password='" + password + '\'' +
-                '}';
-    }
 }
