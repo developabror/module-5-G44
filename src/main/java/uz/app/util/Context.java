71cd956f144836befadacd9905cba40d0b9e5203
Abror Abduganiev <developabror@gmail.com>
2024-06-29 11:52:56 +0500

    test mm branch:

--- src/main/java/uz/app/util/Context.java
+++ src/main/java/uz/app/util/Context.java
@@ -1,12 +1,12 @@
 package uz.app.util;
 
+import lombok.Setter;
 import uz.app.entity.User;
 
 public class Context {
+    @Setter
     private static User user;
-    public static void setUser(User currentUser){
-        user = currentUser;
-    }
+
     public static User getCurrenctUser(){
         return user;
     }
